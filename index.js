de18a315b82467dd6d94bb6fec8d54af96d007e1
haandev <62476477+haandev@users.noreply.github.com>
2022-03-19 11:12:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,16 +13,15 @@ console.log("http server listening on %d", port)
 
 var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
-
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
-
-  console.log("websocket connection open")
-
-  ws.on("close", function() {
+const clients = [
+wss.on("connection", function(client) {
+  clients.push(client)
+  client.on("message".function(data){
+            clients.forEach((c) => c!==client && c.send(payload));
+            })
+  client.on("close", function() {
+    const index = clients.indexOf(client)
+    clients.splice(index,1)
     console.log("websocket connection close")
-    clearInterval(id)
   })
 })
